b2e024d6b9896a8722cfca3b9e9abe8550993aa0
samudraon <samudraon@gmailcom>
2023-03-20 01:39:41 +0600

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,14 @@
+const x = 5;
+
+(x > 5) ? console.log('hello') : console.log('world');
+
+x > 5 ? console.log('greater') : x < 5 ? console.log('less') : console.log('equal');
+
+/* convert number to string */
+let num = 5;
+num = num + '';
+console.log(num)
+
+/* convert string to number */
+num = +num;
+console.log(num);
