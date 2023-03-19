24d70b3aee684a282bbf289305cb4fd8ef1a888e
samudraon <samudraon@gmailcom>
2023-03-19 23:20:15 +0600

    array declare and loop declare added

---
+++ array.js
@@ -0,0 +1,27 @@
+const arr = [1, 2, 3, 4, 5];
+/* for loop */
+// for (i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// /* for of loop */
+// for (let i of arr) {
+//     console.log(i);
+// }
+
+/* for in loop for object*/
+const obj = {
+    name: 'Samudra Karmaker',
+    number: '0234',
+    address: 'Dhaka'
+}
+for (let i in obj) {
+    console.log(obj[i]);
+}
+
+/* while loop */
+// let i = 0;
+
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// };
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+et i = 0;
+
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// };
